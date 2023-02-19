2a00438e81634aeed7ee5383efe2c55e3af463a2
son3cent <85733379+son3cent@users.noreply.github.com>
2023-02-19 16:56:50 +0700

    Update ServerHop.lua

--- Roblox/BloxFruit/ServerHop.lua
+++ Roblox/BloxFruit/ServerHop.lua
@@ -99,5 +99,6 @@ end
 -- resume(create(function()
 connection = game:GetService("RunService").RenderStepped:Connect(onRenderStepped)
 
+-- Fuck
 local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
 queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/son3cent/Lua-Knowleage/main/Roblox/BloxFruit/ServerHop.lua'))()")
