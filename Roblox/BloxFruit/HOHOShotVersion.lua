dcb85eec72f40022d5dcead80b964630c5d66f88
son3cent <85733379+son3cent@users.noreply.github.com>
2023-03-10 00:08:45 +0700

    Update HOHOShotVersion.lua

--- Roblox/BloxFruit/HOHOShotVersion.lua
+++ Roblox/BloxFruit/HOHOShotVersion.lua
@@ -1,1 +1,8 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/BloxFruitFree.lua'))()
+local path = "loadstring(game:HttpGet('https://raw.githubusercontent.com/son3cent/Lua-Knowleage/main/Roblox/BloxFruit/HOHOShotVersion.lua'))()"
+if getgenv().Pirate then
+	path = "getgenv().Pirate = "..tostring(getgenv().Pirate).."\n"..path
+end
 
+local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
+queueteleport(path)
